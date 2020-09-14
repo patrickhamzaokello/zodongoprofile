35d93385ca466ab1cbaf73789d26c08c1a8df5af
Brianoop <brianangoda99@gmail.com>
2020-09-15 01:17:40 +0300

    clean terminal

--- src/header.js
+++ src/header.js
@@ -7,7 +7,7 @@ function header() {
         <div>
 
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Zodongo</a>
+                <a class="navbar-brand" href="#home">Zodongo</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
