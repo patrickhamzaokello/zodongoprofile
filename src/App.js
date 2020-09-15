39de0771b4f680e387754b621e8ebc65809a5509
Brianoop <brianangoda99@gmail.com>
2020-09-15 14:07:26 +0300

    firebase init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from "./header.js"
+// import Header from "./header.js"
 import Content from "./body/Content.js"
 import "bootstrap/dist/css/bootstrap.css"
 import "./App.css"
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="components" >
 
-      <Header />
+      {/* <Header /> */}
       <Content />
 
     
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCs6Xcww303I02gbimze-C1sxHSOrkmSXc",
+    authDomain: "zodongoprofile.firebaseapp.com",
+    databaseURL: "https://zodongoprofile.firebaseio.com",
+    projectId: "zodongoprofile",
+    storageBucket: "zodongoprofile.appspot.com",
+    messagingSenderId: "335254974842",
+    appId: "1:335254974842:web:303e6e13235c16c2db66c3",
+    measurementId: "G-LYVPP40Y2P"
+  };
